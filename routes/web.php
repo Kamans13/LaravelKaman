1b1fd30456c96f60300958e91504c126af81d8cd
Kamans13 <Kaman.septia874@gmail.com>
2019-01-30 10:22:28 +0700

    tokap

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteS
 | contains the "web" middleware group. Now create something great!
 |
 */
@@ -14,6 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function(){
+	Route::get('/', function(){
+		return view('admin.main');
+	})->name('admin.home');
+});
+
 
 Auth::routes();
 
